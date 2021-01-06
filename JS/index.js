e02579e361050cab7f97d316df8f6c1ae908eea9
paulinasadlowska <paulina_sadlowska@wp.pl>
2021-01-06 19:37:34 +0100

    first commit

---
+++ JS/index.js
@@ -0,0 +1,30 @@
+$(".a-about").on("click", function ()
+    {
+        $("body,html").animate(
+            {scrollTop: $("#about").offset().top}, 500
+        )
+    })
+    $(".a-prieces").on("click", function ()
+    {
+        $("body,html").animate(
+            {scrollTop: $("#prieces").offset().top}, 500
+        )
+    })
+    $(".a-gallery").on("click", function ()
+    {
+        $("body,html").animate(
+            {scrollTop: $("#gallery").offset().top}, 500
+        )
+    })
+    $(".a-contact").on("click", function ()
+    {
+        $("body,html").animate(
+            {scrollTop: $("#contact").offset().top}, 500
+        )
+    })
+    $(".reservation").on("click", function ()
+    {
+        $("body,html").animate(
+            {scrollTop: $("#contact").offset().top}, 500
+        )
+    })
